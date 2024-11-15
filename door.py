1a770c2a9e9f03738a854b8fb1b91df395c06ebf
dana kristine <2025miller.dana@fcboe.org>
2024-11-15 13:41:28 -0500

    Update door.py

--- door.py
+++ door.py
@@ -1,1 +1,30 @@
+#imports
+import board
+from neopixel import NeoPixel
+import time
+import random
 
+#colors
+red = (230,15,0)
+green = (0,255,0)
+blue = (0,0,255)
+yellow = (255,50,0)
+cyan = (0,255,255)
+magenta = (255,0,255)
+pink = (255,53, 127)
+purple = (76,4,153)
+lightGreen = (70,247,70)
+orange = (250,75,5)
+darkGreen = (1, 40, 2)
+white = (255,255,255)
+
+#functions
+def candyCane(color, color2):
+  
+
+
+
+
+#main
+while True:
+  candyCane(red, white)
