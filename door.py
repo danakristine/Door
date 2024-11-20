70837fc6b7d3bd2bb035685a76832685329a864b
dana kristine <2025miller.dana@fcboe.org>
2024-11-20 13:31:26 -0500

    Update door.py

--- door.py
+++ door.py
@@ -88,6 +88,80 @@ def wipe():
             time.sleep(0.1)
 
 
+
+def wipee(color):
+    for i in range(np.n):
+        np[i] = color
+        np.show()
+        time.sleep(0.06)
+        
+def reverseWipe(color):
+    for i in range(np.n - 1, -1, -1):
+        np[i] = color
+        np.show()
+        time.sleep(0.06)
+        
+def sparkle(baseColor, sparkleColor, speed, numSpark):
+    np.fill(baseColor)                                                                                           
+    for i in range(numSpark):
+        y = random.randint(0, 29)
+        np[y] = sparkleColor
+        time.sleep(speed)
+    np.show()
+
+y = 0
+def dots(dotColor = darkGreen, black = (0,0,0), endColor = red, i = 0, b = (np.n - 1), timesleep = 0.1, count = 0):
+    """
+    two dots meet halfway in the center then once cross paths they fill the neopixels with the endColor as they reach the ends
+    
+    parameters:
+    dotColor (tuple): the rgb sequence of a specific color: set to orange
+    black (tuple): the rgb sequence of a specific color: set to (0,0,0)
+    endColor (tuple): the rgb sequence of a specific color: set to purple
+    i (int): set to 0
+    b (int): set to # of pixels - 1
+    timesleep (double): set to 0.03
+    count (int): set to 0
+    
+    returns:
+    n/a
+    """
+    global y
+    for x in range(np.n):
+        np[i] = (dotColor)
+        np[(i - 1) % np.n] = black
+        i = (i + 1) % np.n
+        
+        np[b] = (dotColor)
+        np[(b + 1) % np.n] = black
+        b = (b - 1) % np.n
+        np.show()
+        time.sleep(timesleep) 
+	
+        if(i == (np.n / 2)):
+            for l in range(np.n - 1):
+                if l % 2 == 0:
+                    np[l] = red
+                else:
+                    np[l] = green
+            np.show()
+
+        if(y % 2 == 1):
+            black = (0,0,0)
+            dotColor = darkGreen
+        
+    wipee(red)
+    for i in range(13):
+        sparkle(red, white, 0.015, 5)
+    np.fill(red)
+    time.sleep(0.2)
+    reverseWipe(green)
+    for i in range(13):
+        sparkle(green, white, 0.015, 5)
+    np.fill(green)
+    time.sleep(0.2)
+
 #main
 while True:
-  candyCane(white, red)
+    candyCane(white, red)
+    dots()
